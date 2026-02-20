3a3bdce9363acf59eb23c572bc8d7ffc4020af28
Henrick <henrickhrk0903@gmail.com>
2026-02-20 20:16:10 -0300

    first commit

---
+++ src/main/java/com/itb/tcc/mif3an/pizzariadomario/model/entity/Usuario.java
@@ -0,0 +1,18 @@
+package com.itb.tcc.mif3an.pizzariadomario.model.entity;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String password;
+    private String sexo;
+    private String logradouro;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private boolean codStatus;
+    private String uf;
+
+}
